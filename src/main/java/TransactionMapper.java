9c1c38cc228c264813f565bb94d158ac216e70d1
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-16 19:01:00 +0300

    proposed compact solution

--- src/main/java/TransactionMapper.java
+++ src/main/java/TransactionMapper.java
@@ -1,6 +1,5 @@
 import model.Transaction;
 
-import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,22 +7,9 @@ import java.util.stream.Collectors;
 public class TransactionMapper {
     public Map<String, Double> getAverageAmountPerEmailAddress(List<Transaction> transactions) {
 
-        Map<String, List<Transaction>> transactionPerEmail = transactions.stream()
-                .collect(Collectors.groupingBy(Transaction::getEmailAddress));
-
-        Map<String, Double> averageTransactionPerEmail = transactionPerEmail.entrySet().stream()
-                .map(entry1 -> {
-                    String email = entry1.getKey();
-                    List<Transaction> personTransactions = entry1.getValue();
-
-                    double average = personTransactions.stream()
-                            .mapToDouble(Transaction::getAmount)
-                            .average()
-                            .getAsDouble();
-
-                    return new SimpleEntry<>(email, average);
-                })
-                .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
+        Map<String, Double> averageTransactionPerEmail = transactions.stream()
+                .collect(Collectors.groupingBy(Transaction::getEmailAddress,
+                        Collectors.averagingDouble(Transaction::getAmount)));
 
         return averageTransactionPerEmail;
     }
