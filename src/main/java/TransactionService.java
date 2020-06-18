0f99c4b060c46ee3a5c68210482d01b4af387755
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-18 17:58:11 +0300

    amends proposed solutions

--- src/main/java/TransactionService.java
+++ src/main/java/TransactionService.java
@@ -1,6 +1,12 @@
+import model.Person;
+import model.Transaction;
 import repositories.TransactionRepository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TransactionService {
     private final PersonsService personsService;
@@ -12,6 +18,17 @@ public class TransactionService {
     }
 
     List<String> getPersonRolesOfAllTransactions() {
-        throw new UnsupportedOperationException();
+        return transactionRepository.getTransactions()
+                .stream()
+                .filter(Objects::nonNull)
+                .map(Transaction::getEmailAddress)
+                .map(personsService::getPersonByEmailAddress)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(Person::getRoles)
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+
     }
 }
--- src/main/java/repositories/TransactionRepositoryImpl.java
+++ src/main/java/repositories/TransactionRepositoryImpl.java
@@ -1,10 +1,15 @@
 package repositories;
 
 import daos.TransactionDAO;
+import daos.entities.TransactionEntity;
 import model.Transaction;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TransactionRepositoryImpl implements TransactionRepository {
 
@@ -16,11 +21,36 @@ public class TransactionRepositoryImpl implements TransactionRepository {
 
     @Override
     public List<Transaction> getTransactions() {
-        throw new UnsupportedOperationException();
+        return transactionDAO.getTransactions().stream()
+                .map(this::convertToTransaction)
+                .collect(Collectors.toUnmodifiableList());
     }
 
     @Override
     public List<Transaction> getTransactions(LocalDateTime from) {
-        throw new UnsupportedOperationException();
+
+        ZonedDateTime zonedDateTime = from.atZone(ZoneId.of("Europe/Athens"));
+        Instant fromInstant = zonedDateTime.toInstant();
+
+        return transactionDAO.getTransactions()
+                .stream()
+                .filter(transactionEntity -> transactionEntity.getDate().isAfter(fromInstant))
+                .map(this::convertToTransaction)
+                .collect(Collectors.toList());
     }
+
+    private Transaction convertToTransaction(TransactionEntity transactionEntity) {
+        Transaction transaction = new Transaction();
+
+        transaction.setEmailAddress(transactionEntity.getEmail());
+        transaction.setAmount(transactionEntity.getAmount());
+
+        Instant timestamp = transactionEntity.getDate();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(timestamp, ZoneId.of("Europe/Athens"));
+
+        transaction.setDate(localDateTime);
+
+        return transaction;
+    }
+
 }
